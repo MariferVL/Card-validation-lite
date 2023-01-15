7662c3bb3b5552a2c331b40d2745e4658d65b6cb
Marifer <mfvl.1186@gmail.com>
2023-01-15 18:52:12 -0300

    TODO added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 
-// TODO Permitir insertar número de tarjeta 
-// TODO solo dígitos
-// TODO campo requerido
-// TODO validar número
-// TODO  imprimir si es "válida" o no
+// TODO: Permitir insertar número de tarjeta 
+// TODO: solo dígitos
+// TODO: campo requerido
+// TODO:  validar número
+// TODO:  imprimir si es "válida" o no
 // TODO
 // TODO
 
