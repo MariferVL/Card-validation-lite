50714eb0c526d0c3199fdde776b29ed2eea0698f
Marifer <mfvl.1186@gmail.com>
2023-01-15 18:20:43 -0300

    external js added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 
-   // TODO Permitir insertar número de tarjeta 
-   // TODO solo dígitos
-   // TODO campo requerido
-   // TODO validar número
-   // TODO  imprimir si es "válida" o no
-   // TODO
-   // TODO
+// TODO Permitir insertar número de tarjeta 
+// TODO solo dígitos
+// TODO campo requerido
+// TODO validar número
+// TODO  imprimir si es "válida" o no
+// TODO
+// TODO
 
+
+function validate() {
+  const valid = isValid(document.getElementById("cardnumber").value)
+  if (valid) {
+    // Show success in div#result
+  }
+  else {
+    // Show error message in div#result
+  }
+}
+
+function isValid(creditCardNumber) {
+  // is creditCardNumber valid?
+
+  
+  return false
+}
