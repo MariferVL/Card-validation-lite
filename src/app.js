55c6e2d54568111661b90285411f3400b8b58cfc
Marifer <mfvl.1186@gmail.com>
2023-01-15 23:38:50 -0300

    primera parte terminada

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 
-// TODO: solo dígitos
-// TODO:  validar número
-// TODO:  imprimir si es "válida" o no
-// TODO
-// TODO
-
-
-
-const valid = false;
+// Test Data: 4137894711755904
 
 const dangerDiv = document.getElementById("danger");
 const successDiv = document.getElementById("success");
@@ -23,7 +15,7 @@ let description;
 
 function validate() {
   //Get card number from user
-  // const valid = isValid(document.getElementById("cardnumber").value)
+  const valid = isValid(document.getElementById("cardnumber").value)
   if (valid) {
     //Show success in div#result
     successDiv.appendChild(tittleBox);
@@ -32,10 +24,10 @@ function validate() {
     replacement.replaceWith(successDiv);
 
     result = "Tarjeta Válida";
-    description = "Operación exitosa. </br>La información bancaria ingresada es correcta.";
+    description = "Operación exitosa.<br>La información bancaria ingresada es correcta.";
   }
   else {
-    // TODO: Show error message in div#result
+    // Show error message in div#result
     dangerDiv.appendChild(tittleBox);
     dangerDiv.appendChild(msgBox);
     document.getElementById("danger").style.display = "inline";
@@ -56,20 +48,54 @@ function validate() {
 }
 
 function isValid(creditCardNumber) {
+  let valid = false;
 
-   // Validate if card number is integer
-  if (Number.isInteger(creditCardNumber)){
-    //TODO: Split input
+  // Validate if card number is integer
+  if (Number.isInteger(Number(creditCardNumber))) {
+    // Split input
+    const cardDigits = creditCardNumber.split('');
 
-    //TODO: 
+    let stringToNum = cardDigits.map(function(str) {
+      // using map() to convert array of strings to numbers
+      return parseInt(str); });
 
+    // Double value of every 2nd digit
+    for (let index = 0; index < stringToNum.length; index += 2) {
+      const doubleNum = Number(stringToNum[index]) * 2;
+      stringToNum[index] = doubleNum;
+
+      // Check each doubleNum if is greater than 9
+      if (doubleNum > 9) {
+
+        // Add the digits in doubleNum
+        const digitsSum = sumDigits(doubleNum);
+        stringToNum[index] = digitsSum;
+
+      }
+    }
+    console.log("Lista post suma dig: ",stringToNum);
+
+    // Calculate the sum using forEach
+    let sumCardDigits = 0;
+    stringToNum.forEach(x => {sumCardDigits += x;});
+    console.log("Total digitos = " + sumCardDigits);
+
+    //TODO: Check if the sum of all digits is 0
+    if (sumCardDigits % 10 === 0) {
+      valid = true;
+    }
 
   } else {
-    alert("En 'Número de' debes ingresar sólo dígitos.")
+    alert("En 'Número de Tarjeta' debes ingresar sólo dígitos.")
   }
-  // is creditCardNumber valid?
-  //
+  console.log("Resultado de valid :",valid);
 
-
-  return false
+  return valid
 }
+
+
+function sumDigits(n) {
+  return (--n % 9) + 1;
+}
+
+
